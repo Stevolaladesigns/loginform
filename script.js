3a166e6332125d56c73f3c2bec02e92b731dbcbe
Stephen  Kwasi Onumah <onumahbaba@gmail.com>
2023-01-18 11:52:49 +0000

    login form

---
+++ script.js
@@ -0,0 +1,5 @@
+$(".info-item .btn").click(function(){
+    $(" container"). toggleClass ("log-in");
+    });
+    $(" .container-form btn").click(function(){
+         $(".container") .addClass("active"); });
